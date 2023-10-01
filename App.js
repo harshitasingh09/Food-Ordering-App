985a09420bf6bc9b1b52aff24ae8ed5963747f4f
harshita <harshita.singh@mcbirdtechnologies.com>
2023-10-01 11:16:58 +0530

    Parcel bundler

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-const heading = React.createElement("div", {id:'parent'}, 
+import React from 'react';
+import ReactDOM from "react-dom/client"
+
+
+const parent = React.createElement("div", {id:'parent'}, 
 React.createElement("div", {id:'child'}, 
-[React.createElement("h1", {id:'parent'}, "hello world from react h1"),
+[React.createElement("h1", {id:'parent'}, "hello world lets learn react "),
 React.createElement("h2", {id:'parent'}, "hello world from react h2")]));
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,15 @@
+#Parcel
+- build dec
+- HMR= Hot Module Replacement
+- Local Server 
+- faster bulids by caching
+- image optimisation
+- minification
+- bundling 
+- differential bundling to support older browsers
+- consistant hashing
+- error handling 
+- diagnostics
+- tree shaking - remove unused code 
+- HTTPs
+- File watching algo
